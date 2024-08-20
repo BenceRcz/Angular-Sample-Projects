f8ace15beb33384ac0df9ad502882ef2c4384520
Bence Racz <racz.bence@yahoo.com>
2024-08-20 16:01:54 +0300

    persistence changes

--- appointment-app/src/app/appointment-list/appointment-list.component.ts
+++ appointment-app/src/app/appointment-list/appointment-list.component.ts
@@ -1,15 +1,23 @@
 import { Component } from '@angular/core';
 import { Appointment } from '../models/appointment';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-appointment-list',
   templateUrl: './appointment-list.component.html',
   styleUrl: './appointment-list.component.css'
 })
-export class AppointmentListComponent {
-  newAppointmentTitle: string = ""
-  newAppointmentDate: Date = new Date()
-  appointments: Appointment[] = [] 
+export class AppointmentListComponent implements OnInit {
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+  appointments: Appointment[] = [];
+
+  ngOnInit(): void {
+    if (typeof localStorage !== 'undefined') {
+      let savedAppointments = localStorage.getItem("appointments");
+      this.appointments = savedAppointments? JSON.parse(savedAppointments) : [];
+    }
+  }
 
   addAppointment() {
     if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
@@ -18,15 +26,15 @@ export class AppointmentListComponent {
         title: this.newAppointmentTitle,
         date: this.newAppointmentDate
       }
-      this.appointments.push(newAppointment)
-      this.newAppointmentDate = new Date()
-      this.newAppointmentTitle = ""
+      this.appointments.push(newAppointment);
+      this.newAppointmentDate = new Date();
+      this.newAppointmentTitle = "";
     }
-    localStorage.setItem("appointments", JSON.stringify(this.appointments))
+    localStorage.setItem("appointments", JSON.stringify(this.appointments));
   }
 
   deleteAppointment(appointmentId: number) {
-    this.appointments.splice(appointmentId, 1)
-    localStorage.setItem("appointments", JSON.stringify(this.appointments))
+    this.appointments.splice(appointmentId, 1);
+    localStorage.setItem("appointments", JSON.stringify(this.appointments));
   }
 }
